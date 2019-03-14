5a44df7fdb65d574118bbfea76154332dc0201d5
alexisjohnsonn <profileplastics.dev@gmail.com>
2019-03-14 15:33:54 +0000

    Victor's changes lol

--- src/is18285287.java
+++ src/is18285287.java
@@ -19,8 +19,8 @@ public class is18285287{
         init = getStateFromUserInput(initMessage);
         String goalMessage = "Now, enter a final state.";
         goal = getStateFromUserInput(goalMessage);
-		dimension = (int) Math.sqrt(size+1);
-        
+        dimension = (int) Math.sqrt(size+1);
+
     }
 
     private static ArrayList<Integer> getStateFromUserInput(String message)
@@ -54,7 +54,7 @@ public class is18285287{
             // if count != size+1, then there are not enough values in the input string
             // if scanner.hasNextInt(), then there are too many values in the input string
             // if !uniqueIntegers(retVal), then the input values are not unique numbers from 0 to size
-            if (count != size + 1 || scanner.hasNextInt() || !uniqueIntegers(retVal))
+            if (count != size + 1 || scanner.hasNext() || !uniqueIntegers(retVal))
             {
                 input = JOptionPane.showInputDialog(frame, "Your input was not valid. Please re-enter the state. " + instructions);
             }
@@ -67,31 +67,31 @@ public class is18285287{
         }
         return retVal;
     }
-    
+
     private static int calculateH(ArrayList<Integer> testState){
-		// totalDist cumulates the distance of each tile, giving h
-		int totalDist = 0;
-		
-		for(int i = 1; i < testState.size(); i++){
-			// find the index of tile i in the current state
-			int curIdx = testState.indexOf(i);
-			
-			// find the index of tile i in the goal state
-			int goalIdx = goal.indexOf(i);
-			
-			// a piece's column is given by its index in the array % the number of columns,
-			// so number of horizontal moves is given by the difference between the current and goal columns
-			int horizMoves = Math.abs((getCol(curIdx)) - (getCol(goalIdx)));
-			
-			// a piece's row is given by its index in the array / the number of rows,
-			// so number of vertical moves is given by the difference between the current and goal rows
-			int vertMoves = Math.abs((getRow(curIdx)) - (getRow(goalIdx)));
-			
-			totalDist += horizMoves + vertMoves;
-			
-		}
-		
-		return totalDist;
+        // totalDist cumulates the distance of each tile, giving h
+        int totalDist = 0;
+
+        for(int i = 1; i < testState.size(); i++){
+            // find the index of tile i in the current state
+            int curIdx = testState.indexOf(i);
+
+            // find the index of tile i in the goal state
+            int goalIdx = goal.indexOf(i);
+
+            // a piece's column is given by its index in the array % the number of columns,
+            // so number of horizontal moves is given by the difference between the current and goal columns
+            int horizMoves = Math.abs((getCol(curIdx)) - (getCol(goalIdx)));
+
+            // a piece's row is given by its index in the array / the number of rows,
+            // so number of vertical moves is given by the difference between the current and goal rows
+            int vertMoves = Math.abs((getRow(curIdx)) - (getRow(goalIdx)));
+
+            totalDist += horizMoves + vertMoves;
+
+        }
+
+        return totalDist;
     }
 
     private static boolean uniqueIntegers(ArrayList<Integer> arr)
@@ -114,72 +114,51 @@ public class is18285287{
         }
         return isValid;
     }
-    
+
     public static int getRow(int idx){
-    	return idx / dimension;
+        return idx / dimension;
     }
-    
+
     public static int getCol(int idx){
-    	return idx % dimension;
+        return idx % dimension;
     }
-    
+
     public static void getMoves(ArrayList<Integer> currentState){
-    	int idx = currentState.indexOf(0);
-    	int emptyRow = getRow(idx);
-    	int emptyCol = getCol(idx);
-    	
-    	// above
-    	int above = emptyRow - 1; // move is legal if above > 0
-    	if(above > 0){
-    		System.out.println(currentState.get(idx - dimension) + " to the south");
-    	}
-    	
-    	// below
-    	int below = emptyRow + 1; // move is legal if below < dimension
-    	if(below < dimension){
-    		System.out.println(currentState.get(idx + dimension) + " to the north");
-    	}
-    	
-    	// left
-    	int left = emptyCol - 1; // move is legal if left > 0
-    	if(left > 0){
-    		System.out.println(currentState.get(idx-1) + " to the east");
-    	}
-    	
-    	// right
-    	int right = emptyCol +1; // move is legal if right < dimension
-    	if(right < dimension){
-    		System.out.println(currentState.get(idx+1) + " to the west");
-    	}
-    }
+        int idx = currentState.indexOf(0);
+        int emptyRow = getRow(idx);
+        int emptyCol = getCol(idx);
 
-    private static int calculateH(ArrayList<Integer> current, ArrayList<Integer> goal){
-        // assuming the puzzle is a square, the vertical and horizontal dimensions are the same
-        int dimension = (int) Math.sqrt(current.size());
-
-        // totalDist accumulates the distance of each tile, giving h
-        int totalDist = 0;
-
-        for(int i = 1; i < current.size(); i++){
-            // find the index of tile i in the current state
-            int curIdx = current.indexOf(i);
-
-            // find the index of tile i in the goal state
-            int goalIdx = goal.indexOf(i);
-
-            // a piece's column is given by its index in the array % the number of columns,
-            // so number of horizontal moves is given by the difference between the current and goal columns
-            int horizMoves = Math.abs((curIdx % dimension) - (goalIdx % dimension));
-
-            // a piece's row is given by its index in the array / the number of rows,
-            // so number of vertical moves is given by the difference between the current and goal rows
-            int vertMoves = Math.abs((curIdx / dimension) - (goalIdx / dimension));
-
-            totalDist += horizMoves + vertMoves;
-
+        // above
+        int above = emptyRow - 1; // move is legal if above >= 0
+        if(above >= 0){
+            System.out.println(currentState.get(idx - dimension) + " to the south");
+            ArrayList<Integer> heuristicList = move(currentState, idx, idx - dimension);
+            System.out.println("h: " + calculateH(heuristicList));
         }
 
-        return totalDist;
+        // below
+        int below = emptyRow + 1; // move is legal if below < dimension
+        if(below < dimension){
+            System.out.println(currentState.get(idx + dimension) + " to the north");
+            ArrayList<Integer> heuristicList = move(currentState, idx, idx + dimension);
+            System.out.println("h: " + calculateH(heuristicList));
+        }
+
+        // left
+        int left = emptyCol - 1; // move is legal if left >= 0
+        if(left >= 0){
+            System.out.println(currentState.get(idx-1) + " to the east");
+            ArrayList<Integer> heuristicList = move(currentState, idx, idx - 1);
+            System.out.println("h: " + calculateH(heuristicList));
+        }
+
+        // right
+        int right = emptyCol +1; // move is legal if right < dimension
+        if(right < dimension){
+            System.out.println(currentState.get(idx+1) + " to the west");
+            ArrayList<Integer> heuristicList = move(currentState, idx, idx + 1);
+            System.out.println("h: " + calculateH(heuristicList));
+        }
     }
 
     // curIdx is the current index of the 0 tile. newIdx is the new index of the 0 tile in the next move
@@ -190,11 +169,63 @@ public class is18285287{
         return nextMove;
     }
 
+    private static void printState(ArrayList<Integer> current){
+        // TODO seriously
+        System.out.println("Initial:");
+        for(int i = 0; i <= current.size() - dimension; i+=dimension){
+            System.out.print(current.get(i) + "  ");
+            System.out.print(current.get(i+1) + "  ");
+            System.out.println(current.get(i+2) + "  ");
+//			System.out.println(current.get(i+3));
+        }
+        System.out.println();
+    }
 
 
     public static void main(String args[])
     {
-        setUpGame();
+        // setUpGame();
+
+        dimension = 3;
+
+        goal = new ArrayList<>();
+        goal.add(1);
+        goal.add(2);
+        goal.add(3);
+        goal.add(4);
+        goal.add(5);
+        goal.add(6);
+        goal.add(7);
+        goal.add(8);
+//		goal.add(9);
+//		goal.add(10);
+//		goal.add(11);
+//		goal.add(12);
+//		goal.add(13);
+//		goal.add(14);
+//		goal.add(15);
+        goal.add(0);
+
+        ArrayList<Integer> current = new ArrayList<>();
+        current.add(4);
+        current.add(2);
+        current.add(3);
+        current.add(1);
+        current.add(5);
+        current.add(9);
+        current.add(7);
+        current.add(8);
+        current.add(0);
+//		current.add(11);
+//		current.add(10);
+//		current.add(12);
+//		current.add(13);
+//		current.add(15);
+//		current.add(14);
+//		current.add(0);
+
+        printState(current);
+
+        getMoves(current);
     }
 }
-
